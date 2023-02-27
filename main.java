1209a350b73de30c2cf154982929f6c1457255bc
Berru Kaya <125195172+BerruKaya@users.noreply.github.com>
2023-02-27 22:04:08 +0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,16 @@
+class HelloWorld {
+    public static void main(String[] args) {
+      char letter='A';
+      
+    switch(letter){
+      case 'A':
+      case 'I':
+      case 'U':
+      case 'O':
+         System.out.println("Kalın sesli harf");
+         break;
+      default:
+          System.out.println("İnce sesli harf");
+
+}
+}
